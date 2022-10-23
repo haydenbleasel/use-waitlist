cdd845d898ce896563df17e7f94509dd4562a209
Hayden Bleasel <hello@haydenbleasel.com>
2022-10-23 12:57:34 -0400

    Add getWaiter endpoint

--- src/waitlist.ts
+++ src/waitlist.ts
@@ -20,6 +20,11 @@ type WaiterProps = {
   waitlist_id: string;
 };
 
+type GetWaiterProps = {
+  email?: string;
+  uuid?: string;
+};
+
 const fetcher = async (url: string) =>
   fetch(url, {
     method: 'GET',
@@ -40,12 +45,37 @@ const addWaiterToWaitlist = async (api_key: string, email: string) => {
   }>;
 };
 
+const getWaiterFromWaitlist = async (
+  api_key: string,
+  props: GetWaiterProps
+) => {
+  let endpoint = '';
+
+  if (props.email) {
+    endpoint = createEndpoint(
+      `/waitlist?api_key=${api_key}&email=${props.email}`
+    );
+  } else if (props.uuid) {
+    endpoint = createEndpoint(
+      `/waitlist?api_key=${api_key}&uuid=${props.uuid}`
+    );
+  } else {
+    throw new Error('Must provide either email or uuid');
+  }
+
+  await fetch(endpoint, {
+    method: 'GET',
+    headers,
+  });
+};
+
 const useWaitlist = (
   api_key: string
 ): {
   data: WaitlistProps | undefined;
   error: Error | undefined;
   addWaiter: (email: string) => Promise<{ waiter: WaiterProps }>;
+  getWaiter: (props: GetWaiterProps) => Promise<void>;
 } => {
   const endpoint = createEndpoint(`/waitlist?api_key=${api_key}`);
   const { data, error } = useSWR<WaitlistProps, Error>(endpoint, fetcher);
@@ -53,7 +83,10 @@ const useWaitlist = (
   const addWaiter = async (email: string) =>
     addWaiterToWaitlist(api_key, email);
 
-  return { data, error, addWaiter };
+  const getWaiter = async (props: GetWaiterProps) =>
+    getWaiterFromWaitlist(api_key, props);
+
+  return { data, error, addWaiter, getWaiter };
 };
 
 export default useWaitlist;
